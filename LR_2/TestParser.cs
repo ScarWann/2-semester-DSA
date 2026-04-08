e2af3becddeee2750df09c4640abe301e7db66ec
ScarWann <160788504+ScarWann@users.noreply.github.com>
2026-04-09 01:51:51 +0300

    Update TestParser.cs

--- LR_2/TestParser.cs
+++ LR_2/TestParser.cs
@@ -2,66 +2,103 @@ namespace DSA_2;
 
 public class TestParser
 {
-    private readonly int testCount;
-    private readonly int testLength;
     private bool calculatedInversions;
-    private int[] inversions;
+    private (int Id, int Inversions)[]? indexedInversions;
     private bool appliedMask;
     private int appliedMaskId = -1;
-    private string filename;
+    private TestCase[]? data;
 
-    public TestParser(string filename)
+#pragma warning disable CS8618
+    public TestParser()
     {
-        string[] lines = File.ReadAllLines(filename);
-        var firstLine = lines[0].Split(' ');
-        if (firstLine.Length != 2) throw new FormatException("First line must contain two values separated by a whitespace.");
+    }
+#pragma warning restore CS8618
 
-        int testCount = int.Parse(firstLine[0], System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture);
-        int testLength = int.Parse(firstLine[1], System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture);
-        if (lines.Length != testCount + 1) throw new InvalidDataException("First integer of the first row must be equal to the amount of tests. Either adjust the integer or provide correct test amount.");
-        int[] expectedTestContents = [.. Enumerable.Range(1, testLength)];
+    public IList<TestCase> Data => this.data;
+
+    public int MaskId { get; set; }
+
+    public IReadOnlyCollection<(int Id, int Inversions)> IndexedIversions => this.calculatedInversions ? this.indexedInversions : this.CalculateInversions();
+
+    public string OutputFile { get; set; }
+
+    public string InputFile { get; set; }
+
+    public static void Main(string[] args)
+    {
+        var parser = new TestParser();
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "-i": parser.InputFile = args[++i]; break;
+                case "-o": parser.OutputFile = args[++i]; break;
+                case "-x": parser.MaskId = int.Parse(args[++i], System.Globalization.CultureInfo.InvariantCulture); break;
+                default:
+                    ExitWithMessage(
+                    "Usage: dotnet run -- -i <input> -o <output> [-x: Mask ID]:" +
+                    " '-i' Input file path" +
+                    " '-o' Output file path" +
+                    " '-x' ID of the array relative to which the inversions are counted"); break;
+            }
+        }
+
+        parser.ReadFile();
+        parser.DumpResults();
+    }
+
+    public void ReadFile()
+    {
+        ArgumentNullException.ThrowIfNull(this.InputFile);
+        string[] lines = File.ReadAllLines(this.InputFile);
+
+        ThrowExceptionOnInvalidInputFormatting(lines);
+
+        var firstLine = lines[0].Split(' ');
+        int testCount = int.Parse(firstLine[0], System.Globalization.CultureInfo.InvariantCulture);
         int[][] rawLineData = new int[testCount][];
 
         for (int i = 0; i < testCount; i++)
         {
-            rawLineData[i] = [.. lines[i].Split(' ').Select(str => int.Parse(str, System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture))];
-            if (rawLineData[i].Length != testLength + 1) throw new InvalidDataException("Each line except for the first must contain the amount of integers provided by the second integer of the first row, plus an identifier as the first integer of the row. Either adjust the integer in the first row or provide correct tests of correct size.");
-            if (rawLineData[i][0] != i + 1) throw new InvalidDataException("Each test must start with an identifier, in order.");
-            if (rawLineData[i][1..].ToHashSet() != expectedTestContents.ToHashSet() && rawLineData[i][1..].ToHashSet().Count == rawLineData[i][1..].Length) throw new InvalidDataException("Each test must consist of a range of integers from 1 up to the length of the test.");
+            rawLineData[i] = [.. lines[i + 1].Split(' ').Select(str => int.Parse(str, System.Globalization.CultureInfo.InvariantCulture))];
         }
 
-        this.Data = rawLineData.Select(data => new TestCase(array: data[1..], id: data[0])).ToArray();
-        this.testCount = testCount;
-        this.testLength = testLength;
-        this.filename = filename;
+        this.data = rawLineData.Select(data => new TestCase(array: data[1..], id: data[0])).ToArray();
     }
 
-    public IList<TestCase> Data { get; }
-
-    public int MaskId { get; set; }
-
-    public IReadOnlyCollection<int> Inversions => this.calculatedInversions ? this.inversions : this.CalculateInversions();
-
-    private int[] CalculateInversions(int maskId)
+    public void DumpResults()
     {
-        
+        ArgumentNullException.ThrowIfNull(this.OutputFile);
+        File.WriteAllText(this.OutputFile, this.ToString());
     }
 
-    private int[] CalculateInversionsWithoutMask()
+    private (int Id, int Inversions)[] CalculateInversions()
+    {
+        if (this.MaskId != 0) this.ApplyMask();
+        this.CalculateInversionsWithoutMask();
+
+        foreach (var t in this.Data)
+            Console.WriteLine($"TestCase {t.Id} array after mask: [{string.Join(",", t.ToArray())}] inversions: {t.Inversions}");
+
+        return this.indexedInversions;
+    }
+
+    private (int Id, int Inversions)[] CalculateInversionsWithoutMask()
     {
         this.calculatedInversions = true;
-        return this.inversions = this.Data.Select(testCase => testCase.Inversions).ToArray();
+        return this.indexedInversions = this.Data.Where(e => e.Id != this.appliedMaskId).OrderBy(e => e.Inversions).Select(e => (e.Id, e.Inversions)).ToArray();
     }
 
-    private void ApplyMask(int id)
+    private void ApplyMask()
     {
-        ArgumentOutOfRangeException.ThrowIfNegative(id);
-        this.ApplyMask(this.Data[id]);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(this.MaskId);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(this.MaskId, this.Data.ToArray().Length);
+        this.ApplyMask(this.Data[this.MaskId - 1]);
     }
 
     private void ApplyMask(TestCase testCase)
     {
-        this.appliedMaskId = this.Data.IndexOf(testCase);
+        this.appliedMaskId = testCase.Id;
         this.ApplyMask(indexMask: testCase.ToArray().Select((val, idx) => (val, idx)).ToDictionary(pair => pair.val, pair => pair.idx));
     }
 
@@ -78,13 +115,34 @@ public class TestParser
         }
     }
 
-    public void DumpResults(string filename)
+    private new string ToString()
     {
-        
+        return this.MaskId + $"\n{string.Join("\n", this.IndexedIversions.Select(e => $"{e.Id} {e.Inversions}"))}\n";
     }
 
-    public override string ToString()
+    private static void ThrowExceptionOnInvalidInputFormatting(string[] lines)
     {
-        return this.appliedMaskId != -1 ? $"Unknown mask ID\n{this.}"
+        var firstLine = lines[0].Split(' ');
+        if (firstLine.Length != 2) throw new FormatException("First line must contain two values separated by a whitespace.");
+
+        int testCount = int.Parse(firstLine[0], System.Globalization.CultureInfo.InvariantCulture);
+        int testLength = int.Parse(firstLine[1], System.Globalization.CultureInfo.InvariantCulture);
+        if (lines.Length != testCount + 1) throw new InvalidDataException("First integer of the first row must be equal to the amount of tests. Either adjust the integer or provide correct test amount.");
+        int[] expectedTestContents = [.. Enumerable.Range(1, testLength)];
+        int[][] rawLineData = new int[testCount][];
+
+        for (int i = 0; i < testCount; i++)
+        {
+            rawLineData[i] = [.. lines[i + 1].Split(' ').Select(str => int.Parse(str, System.Globalization.CultureInfo.InvariantCulture))];
+            if (rawLineData[i].Length != testLength + 1) throw new InvalidDataException("Each line except for the first must contain the amount of integers provided by the second integer of the first row, plus an identifier as the first integer of the row. Either adjust the integer in the first row or provide correct tests of correct size.");
+            if (rawLineData[i][0] != i + 1) throw new InvalidDataException("Each test must start with an identifier, in order.");
+            if (!rawLineData[i][1..].Order().SequenceEqual(expectedTestContents.Order())) throw new InvalidDataException("Each test must consist of a range of integers from 1 up to the length of the test.");
+        }
+    }
+
+    private static void ExitWithMessage(string message)
+    {
+        Console.WriteLine(message);
+        Environment.Exit(0);
     }
 }
